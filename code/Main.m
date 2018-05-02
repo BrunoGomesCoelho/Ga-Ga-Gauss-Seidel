97f7f93b9d1832710b85b9ddbb545662b68ef318
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-05-02 13:51:44 -0300

    Start basic main

---
+++ code/Main.m
@@ -0,0 +1,10 @@
+function Main()
+  inp = input ("Escolha 'n' para rodar o algoritmo padrão, 't' para testes: ", "s");
+  if inp == 'n'
+    printf("Rodando algoritmo padrão com");
+    # TODO @Gabriel Cruz
+  else
+    # TODO
+  end
+
+end
