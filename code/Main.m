0a7e00ebf4945cffaf80f945fd78c9870a086154
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-05-02 17:58:19 -0300

    Add mentions for second test

--- code/Main.m
+++ code/Main.m
@@ -1,10 +1,15 @@
 function Main()
   inp = input ("Escolha 'n' para rodar o algoritmo padrão, 't' para testes: ", "s");
   if inp == 'n'
-    printf("Rodando algoritmo padrão com");
-    # TODO @Gabriel Cruz
+    printf("Rodando algoritmo padrão com N = 50\n");
   else
-    # TODO
+    second_inp = input("Escolha 'r' para o teste rápido, 'c' para o teste \ 
+                        completo variando o n em [10, 300] de 10 em 10")
+    if second_inp == 'r':
+      Test();
+    else:
+      Test_Completo();
+    end
   end
 
 end
