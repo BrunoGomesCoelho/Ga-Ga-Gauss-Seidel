205679f48e58cfe9509f933eb0a94f316c659364
BrunoGomesCoelho <brasinhacoelho@hotmail.com>
2018-05-02 15:56:39 -0300

    Update test function

---
+++ code/Test.m
@@ -0,0 +1,19 @@
+function Test()
+  n = 100;
+  error = 10**-10;
+  A = Generate_Imput_Matrix(n);
+  b = Generate_Imput_Vector_c(n);
+
+  # Rodar e calcular tempo
+  t0 = clock ();
+  solved = Gauss_Seidel(A, b, n, intmax-2, error);
+  
+  if solved == true
+    printf("O programa foi corretamente executado e chegou numa resposta\n")
+  else
+    printf("O programa atingiu o maximo de iterações e nao chegou numa resposta satisfatoria\n")
+  end
+  
+  printf("O tempo tempo total foi %d: \n", etime(clock (), t0) )
+
+end
